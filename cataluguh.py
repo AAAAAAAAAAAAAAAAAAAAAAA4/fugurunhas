c3e936f2f070ada7f8cab88794ad5d4b7c70d308
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-08-06 15:16:26 -0300

    bom dia

--- cataluguh.py
+++ cataluguh.py
@@ -1,1 +1,23 @@
 # Catalogo com fulme, album, personagem e figurinhas.###
+
+filmes = []
+
+def cadastras_fiil(titulo, preco_al, preco_pac):
+    filme = {
+        "titulo": titulo,
+        "preco album": preco_al,
+        "preco_pacote": preco_pac,
+        "personagem": [],
+        "figurinhas": []
+    }
+    filmes.append(filme)
+    return filme
+def cadastrar_pers(filme, nome):
+    filme["personegeng"].append(nome)
+
+def cadastrar_figo(filme, numero, personagem)
+    figurinha ={
+        "numero": numero,
+        "personagem": personagem
+    }
+    filme["figurinhas"].append(figurinha)
